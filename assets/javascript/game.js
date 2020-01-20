fa525c69877a6721857ad80c4984f34db4cfee04
Dylan Myers <dylanmyers178@gmail.com>
2020-01-20 13:41:48 -0600

    still is not working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,95 +1,60 @@
 
 
-
-
-
-
-$(document).ready(function(){
+//$document.ready(function() {
 
     var topics = [];
 
+    var x = $(this).data("search");
+	//console.log(x);
 
-    function displayNew() {
-     
-        
-        var x = $(this).data("search");
-        console.log(x);
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + x + "&api_key=FmLvJ70vpE5u9rPiJjIlZ4vAtkgwB7Q3";
 
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + x + "&api_key=FmLvJ70vpE5u9rPiJjIlZ4vAtkgwB7Q3";
-        console.log(queryURL);
+    //console.log(queryURL)
+ function displayGif(){
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).done(function(response){
+        var results = response.data;
+        //console.log(results);
+        for (var i = o; i < results.length; i++) {
+            var newDiv = $("<div class='col-md-4'>");
 
-        $.ajax({
-            url: queryURL,
-            method: "Get"
-        }).done(function(response){
-            var results = response.data;
-            console.log(results)
-
-            for (var i = o; i < results.length; i++) {
-
-                var searchDiv = $("<div class='col-md-4'>");
-                var rating = results[i].rating;
-                var defaultanimation = results[i].images.fixed_height.url;
-                var static = results[i].images.fixed_height_still.url;
-                var showImage = $("<img>");
-                var p = $("<p>").text("Rating: " + rating);
+            var rating= results[i].rating;
+            var showImage = $("<img>");
+            var p = $("<p>").text("rating: " +  rating);
 
 
-
-                showImage.attr("src", static);
-                showImage.attr("data-state", "still");
-                showImage.attr("data-still", static)
-                showImage.attr("data-animate", defaultanimation);
-                searchDiv.append(p);
-                searchDiv.append(showImage);
-                $("#gifs").prepend(searchDiv);
-            };
-        });
-
-
-        $("#addSearch").on("click", function(event){
-            event.preventDefault();
-            var newSearch = $("#newSearch").val().trim();
-            topics.push(newSearch);
-            console.log(topics);
-            $("#newSearch").val('');
-            displayButtons();
-        });
-
-
-
-        function displayButtons() {
-            $("#myButtons").empty();
-            for (var i = 0; i < topics.length; i++) {
-                a.attr("id", "looking");
-                a.attr("data-search", topics[i]);
-                a.text(topics[i]);
-                $("#myButtons").append(a);
-            };
-
+            showImage.addClass("newgiphy");
+            newDiv.append(p);
+            newDiv.append(showImage);
+            $("#gifs").prepend(newDiv);
         };
+    });
 
+    $("#addSearch").on("click", function(event){
+        event.preventDefault();
+        var newSearch = $("#newInput").val().trim();
+        topics.push(newSearch);
+        //console.log(topics);
+        $("newInput").val('');
         displayButtons();
+    });
 
-        $(document).on("click", "#looking", displayNew);
+    function displayButtons() {
+        $("#myButtons").empty();
+        for (var i = 0; i < topics.length; i++) {
+            var a = $('<button class="btn btn-primary">');
+            a.attr("id", "looking");
+            a.attr("data-search", topics[i]);
+            a.text(topics[i]);
+            $("#myButtons").append(a);
+        };
+    };
+ };
+    //displayButtons();
 
-        // function pausePlay() {
-        //     if (state === "still") {
-        //         $(this).attr("src", $(this).attr("data-animate"));
-        //         $(this).attr("data-state", "animate");
-        //       } else {
-        //         $(this).attr("src", $(this).attr("data-still"));
-        //         $(this).attr("data-state", "still");
-        //   };
-
-        // };
-
-
-
-
-
-    }
+    $(document).on("click", "#looking", displayGif);
 
 
-
-})
+//})
